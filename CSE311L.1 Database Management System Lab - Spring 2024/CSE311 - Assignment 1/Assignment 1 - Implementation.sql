01b08fc89e495962f9081c2e45cf09e756a7d8cc
saifmohammednipun <saifmohammednipun@gmail.com>
2024-02-26 19:37:09 +0600

    Added CSE311 Assignment - 1

--- CSE311L.1 Database Management System Lab - Spring 2024/CSE311 - Assignment 1/Assignment 1 - Implementation.sql
+++ CSE311L.1 Database Management System Lab - Spring 2024/CSE311 - Assignment 1/Assignment 1 - Implementation.sql
@@ -1,211 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 26, 2024 at 05:28 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE assignment1_2121913642;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE Teacher(T_id INT NOT NULL,
+                     Name VARCHAR(255),
+                     DOB DATE,
+                     Street VARCHAR(255),
+                     City VARCHAR(255),
+                     Designation VARCHAR(255),
+                     Total_salary DECIMAL(6,2),
+                     PRIMARY KEY (T_id)
+                     );
 
---
--- Database: `assignment1_2121913642`
---
 
--- --------------------------------------------------------
+CREATE TABLE Course(Course_id INT NOT NULL,
+                    Title VARCHAR(255),
+                    Credit_hour INT,
+                    PRIMARY KEY (Course_id)
+                    );
 
---
--- Table structure for table `apartment`
---
 
-CREATE TABLE `apartment` (
-  `Apart_id` int(11) NOT NULL,
-  `Size` int(11) DEFAULT NULL,
-  `Building_name` varchar(255) DEFAULT NULL,
-  `TotalFloor_num` int(11) DEFAULT NULL,
-  `Location` varchar(255) DEFAULT NULL,
-  `Price` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE Teaches(T_id INT NOT NULL,
+                     Course_id INT NOT NULL,
+                     Semester VARCHAR(255),
+                     Year_Teaching YEAR,
+                     Remuneration DECIMAL(4,2),
+                     Section INT,
+                     Number_students INT,
+                     Course_fee DECIMAL(4,2),
+                     PRIMARY KEY (T_id, Course_id,Semester, Year_Teaching),
+                     FOREIGN KEY (T_id) REFERENCES teacher(T_id),
+                     FOREIGN KEY (Course_id) REFERENCES course(Course_id)
+                     );
 
--- --------------------------------------------------------
 
---
--- Table structure for table `child`
---
+CREATE TABLE Mother(M_BRN INT NOT NULL,
+                    Name VARCHAR(255),
+                    Number_sons INT,
+                    NUmber_daughters INT,
+                    Total_child INT,
+                    DOB DATE,
+                    Profession VARCHAR(255),
+                    Income DECIMAL(7,2),
+                    PRIMARY KEY (M_BRN)
+                    );
 
-CREATE TABLE `child` (
-  `C_BRN` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `DOB` date DEFAULT NULL,
-  `BirthTime_height` decimal(5,2) DEFAULT NULL,
-  `BirthTime_weight` decimal(5,2) DEFAULT NULL,
-  `Gender` char(5) DEFAULT NULL,
-  `M_BRN` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+CREATE TABLE Child(C_BRN INT NOT NULL,
+                   Name VARCHAR(255),
+                   DOB DATE,
+                   BirthTime_height DECIMAL(5,2),
+                   BirthTime_weight DECIMAL(5,2),
+                   Gender CHAR(5),
+                   M_BRN INT NOT NULL,
+                   PRIMARY KEY (C_BRN),
+                   FOREIGN KEY (M_BRN) REFERENCES mother(M_BRN)
+                   );
 
---
--- Table structure for table `course`
---
 
-CREATE TABLE `course` (
-  `Course_id` int(11) NOT NULL,
-  `Title` varchar(255) DEFAULT NULL,
-  `Credit_hour` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE Apartment(Apart_id INT NOT NULL,
+                       Size INT,
+                       Building_name VARCHAR(255),
+                       TotalFloor_num INT,
+                       Location VARCHAR(255),
+                       Price VARCHAR(255),
+                       PRIMARY KEY (Apart_id)
+                       );
 
--- --------------------------------------------------------
 
---
--- Table structure for table `mother`
---
+CREATE TABLE Person(NID INT NOT NULL,
+                    Name VARCHAR(255),
+                    Gender CHARACTER(5),
+                    DOB DATE,
+                    Profession VARCHAR(255),
+                    Income DECIMAL(7,2),
+                    Street VARCHAR(255),
+                    City VARCHAR(255),
+                    Birth_registration INT,
+                    Apart_id INT,
+                    PRIMARY KEY (NID),
+                    FOREIGN KEY (Apart_id) REFERENCES apartment(Apart_id)
+                    );
 
-CREATE TABLE `mother` (
-  `M_BRN` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Number_sons` int(11) DEFAULT NULL,
-  `NUmber_daughters` int(11) DEFAULT NULL,
-  `Total_child` int(11) DEFAULT NULL,
-  `DOB` date DEFAULT NULL,
-  `Profession` varchar(255) DEFAULT NULL,
-  `Income` decimal(7,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `person`
---
-
-CREATE TABLE `person` (
-  `NID` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Gender` char(5) DEFAULT NULL,
-  `DOB` date DEFAULT NULL,
-  `Profession` varchar(255) DEFAULT NULL,
-  `Income` decimal(7,2) DEFAULT NULL,
-  `Street` varchar(255) DEFAULT NULL,
-  `City` varchar(255) DEFAULT NULL,
-  `Birth_registration` int(11) DEFAULT NULL,
-  `Apart_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teacher`
---
-
-CREATE TABLE `teacher` (
-  `T_id` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `DOB` date DEFAULT NULL,
-  `Street` varchar(255) DEFAULT NULL,
-  `City` varchar(255) DEFAULT NULL,
-  `Designation` varchar(255) DEFAULT NULL,
-  `Total_salary` decimal(6,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teaches`
---
-
-CREATE TABLE `teaches` (
-  `T_id` int(11) NOT NULL,
-  `Course_id` int(11) NOT NULL,
-  `Semester` varchar(255) NOT NULL,
-  `Year_Teaching` year(4) NOT NULL,
-  `Remuneration` decimal(4,2) DEFAULT NULL,
-  `Section` int(11) DEFAULT NULL,
-  `Number_students` int(11) DEFAULT NULL,
-  `Course_fee` decimal(4,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `apartment`
---
-ALTER TABLE `apartment`
-  ADD PRIMARY KEY (`Apart_id`);
-
---
--- Indexes for table `child`
---
-ALTER TABLE `child`
-  ADD PRIMARY KEY (`C_BRN`),
-  ADD KEY `M_BRN` (`M_BRN`);
-
---
--- Indexes for table `course`
---
-ALTER TABLE `course`
-  ADD PRIMARY KEY (`Course_id`);
-
---
--- Indexes for table `mother`
---
-ALTER TABLE `mother`
-  ADD PRIMARY KEY (`M_BRN`);
-
---
--- Indexes for table `person`
---
-ALTER TABLE `person`
-  ADD PRIMARY KEY (`NID`),
-  ADD KEY `Apart_id` (`Apart_id`);
-
---
--- Indexes for table `teacher`
---
-ALTER TABLE `teacher`
-  ADD PRIMARY KEY (`T_id`);
-
---
--- Indexes for table `teaches`
---
-ALTER TABLE `teaches`
-  ADD PRIMARY KEY (`T_id`,`Course_id`,`Semester`,`Year_Teaching`),
-  ADD KEY `Course_id` (`Course_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `child`
---
-ALTER TABLE `child`
-  ADD CONSTRAINT `child_ibfk_1` FOREIGN KEY (`M_BRN`) REFERENCES `mother` (`M_BRN`);
-
---
--- Constraints for table `person`
---
-ALTER TABLE `person`
-  ADD CONSTRAINT `person_ibfk_1` FOREIGN KEY (`Apart_id`) REFERENCES `apartment` (`Apart_id`);
-
---
--- Constraints for table `teaches`
---
-ALTER TABLE `teaches`
-  ADD CONSTRAINT `teaches_ibfk_1` FOREIGN KEY (`T_id`) REFERENCES `teacher` (`T_id`),
-  ADD CONSTRAINT `teaches_ibfk_2` FOREIGN KEY (`Course_id`) REFERENCES `course` (`Course_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
