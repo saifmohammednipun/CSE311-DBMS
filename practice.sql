5f9a0959c5bac11858c4ff675d95f912d86b9632
saifmohammednipun <saifmohammednipun@gmail.com>
2024-04-23 22:53:10 +0600

    Update CSE311-DBMS

--- practice.sql
+++ practice.sql
@@ -1,33 +1,28 @@
-The relational schema for student course registration are as follows
+-- The relational schema for student course registration are as follows
 
-Parents-S(F-NID, M-NID, Sid, F-name, M-name, street, city, income)
-Student (Sid, name, street, city. Mobile, email, CGPA, age, tot-cred, dept-id)
-Takes (course-id, Sid, semester, year, gradepoint)
-Course (course-id, title, credit-hour)
-Parents-T(F-NID, M-NID, Tid, F-name, M-name, street, city, income)
-Teacher (Tid, name, designation, street, city, Mobile, email, salary, date-of-birth, dept-id)
-Teach (course-id, Tid, semester, year, remuneration)
+-- Parents-S(F-NID, M-NID, Sid, F-name, M-name, street, city, income)
+-- Student (Sid, name, street, city. Mobile, email, CGPA, age, tot-cred, dept-id)
+-- Takes (course-id, Sid, semester, year, gradepoint)
+-- Course (course-id, title, credit-hour)
+-- Parents-T(F-NID, M-NID, Tid, F-name, M-name, street, city, income)
+-- Teacher (Tid, name, designation, street, city, Mobile, email, salary, date-of-birth, dept-id)
+-- Teach (course-id, Tid, semester, year, remuneration)
 
 
-Q. 1: Write SQL expression to find Sid, F-NID, M-NID, mobile, email and age of all
-students whose tot-cred is greater than or equal to 130.
-
-Solution:
-Select Sid, F-NID, M-NID, mobile, email, age
-From Students  Natural Join Parents-S 
-Where tot-cred >= 130;
+-- Q. 1: Write SQL expression to find Sid, F-NID, M-NID, mobile, email and age of all
+-- students whose tot-cred is greater than or equal to 130.
+Select s.Sid, p.F-NID, p.M-NID, s.mobile, s.email, s.age
+From Students s join Parents-S p on s.Sid = p.Sid
+Where s.tot-cred >= 130;
 
 
 Q. 2: Write SQL expression to find F-NID, Sid, course-id and title for all students whose
 parents live in Dhaka.
-
-Solution:
-Select P.F-NID, S.Sid, T.course-id, C.title
-From Student S, Parents-S P, Takes T, Course C
-Where  S.Sid = P.Sid 
-and Sid = T.Sid 
-and T.course-id = C.course-id
-and P.city = 'Dhaka';
+Select p.F-NID, s.Sid, t.course-id, c.title
+From Parents-S p join Student s on p.Sid = s.Sid
+                 join Takes t on s.Sid = t.Sid
+                 join Course c on t.course-id = c.course-id
+Where p.city IN ('Dhaka')
 
 Q. 3: Write SQL expression to find Sid, name, street, city and average gradepoint of each
 student.
@@ -43,7 +38,9 @@ students) living in Dhaka or Rajshahi and average income higher than 500000.
 Solution:
 Select city, street, avg(income), max(income), min(income)
 From Parents-S
-Where city = 'Dhaka' or city = 'Rajshahi' 
+Where city = ( Select city 
+               From Parents-S 
+               Where city = 'Dhaka' or 'Rajshahi')
 Group by city, street;
 Having avg(income) > 500000;
 
@@ -52,13 +49,25 @@ taken any course (course-id) taken by Abid in Fall 2018.
 
 Solution:
 Select S.Sid, S.name, T.course-id, C.title
-From Student S, Takes T, Course C
-Where S.Sid = T.Sid
-and T.course-id = C.course-id
-and T.semester = 'Fall'
-and T.yearf = 2018
-and T.course-id in (Select course-id
-                    From Takes, Student
-                    Where Student.Sid = Takes.Sid)
+From Student S join Takes T on S.Sid = T.Sid
+              join Course C on T.course-id = C.course-id
+Where T.course-id IN (Select course-id
+                      From Takes
+                      Where Sid = (Select Sid
+                                   From Student
+                                   Where name = 'Abid')
+                      And semester = 'Fall'
+                      And year = 2018);
 
-
+Q. 6: Write SQL expression to find Sid, name, course-id and title of all students who have
+taken all courses taken by Abid in Fall 2018.
+Select S.Sid, S.name, T.course-id, C.title
+From Student S join Takes T on S.Sid = T.Sid
+              join Course C on T.course-id = C.course-id
+Where T.course-id NOT Exists(Select course-id
+                      From Takes
+                      Where Sid = (Select Sid
+                                   From Student
+                                   Where name = 'Abid')
+                      And semester = 'Fall'
+                      And year = 2018);
